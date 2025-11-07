6e491d325778953063ff452f66c9bd2436ca7f94
Priyesh Choudhary <143314183+priyeshkr18@users.noreply.github.com>
2025-11-08 00:31:09 +0530

    Add files via upload

---
+++ RA.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class RA {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number of elements: ");
+		int n = sc.nextInt();
+
+
+		int arr[] = new int[n];
+		System.out.print("Enter array elements: ");
+		for(int i=0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+
+		int start = 0, end = n -1;
+		while(start < end) {
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+
+		System.out.println("Reversed array: ");
+		for(int i=0; i<n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
